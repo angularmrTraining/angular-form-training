1bb89973deecba26b92b60e30be37272fdbab68a
Lazhari <lazhari.mohammed@outlook.com>
2018-01-14 20:48:13 +0000

    Add todo component and resources doc component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ContactFormComponent } from './components/contact-form/contact-form.component';
+import { HeaderComponent } from './components/blocks/header/header.component';
+import { ToDosComponent } from './components/blocks/to-dos/to-dos.component';
+import { DocComponent } from './components/blocks/doc/doc.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactFormComponent
+    ContactFormComponent,
+    HeaderComponent,
+    ToDosComponent,
+    DocComponent
   ],
   imports: [
     BrowserModule,
